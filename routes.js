449f9217052f882765a09d8b0cc677e5d96bafc0
Pedro Barcellos <pedrobarcellosdosreis@gmail.com>
2024-10-06 13:50:13 -0300

    Rename import

--- routes.js
+++ routes.js
@@ -1,7 +1,8 @@
 import { default as AppRouter } from "./core/Router.js"
 import HomeController from "./controllers/HomeController.js"
-import TodosController from "./controllers/todos.js"
+import TodosController from "./controllers/TodosController.js"
 
+// TODO: User Service Container to get an instance of core/Router
 const Router = AppRouter.init()
 
 Router.get("/", [HomeController, "index"])
