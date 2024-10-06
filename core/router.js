a5654cb7830072bee3167e877555c0ae5e20fa85
Pedro Barcellos <pedrobarcellosdosreis@gmail.com>
2024-10-06 16:01:50 -0300

    Refactor route registering

--- core/router.js
+++ core/router.js
@@ -1,31 +1,44 @@
+import express from "express"
+import Route from "../models/Route.js"
+
 export default class Router {
   routes = []
+  #router = express.Router()
 
   static init() {
     return new this()
   }
 
-  get(path, [routeController, action]) {
-    this.#addRoute({
-      method: "GET",
-      controller: routeController,
-      path,
-      action,
-    })
+  get length() {
+    return this.routes.length
+  }
+
+  get(path, [controller, action]) {
+    const route = this.#getRoute("get", path, controller, action)
+
+    this.routes.push(route)
 
     return this
   }
 
   registerRoutes(app) {
-    this.routes.forEach(({ path, action, controller }) => {
-      const routeController = new controller()
-      const handler = routeController[action]
-      app.use(path, handler)
-    })
+    this.routes.forEach(this.#register(app))
+    console.log(`[Registered Routes: ${this.length}]`)
+    console.table(this.routes)
   }
 
-  #addRoute(routeProps) {
-    this.routes.push({ ...routeProps, middleware: "none" })
-    console.log("[Routes]\n", this.routes)
+  #register(app) {
+    return ({ method, path, controller, action }) => {
+      const routeController = new controller()
+      const handler = routeController[action]
+
+      this.#router[method](path, handler)
+
+      app.use(path, this.#router)
+    }
+  }
+
+  #getRoute(method, path, controller, action) {
+    return Route.new({ method, path, controller, action, middleware: "none" })
   }
 }
